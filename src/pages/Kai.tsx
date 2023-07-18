33665327e2fff763cc136751048c491b1ce9e804
Arie Varian Akbari <ari.varian@gmail.com>
2023-07-18 13:23:46 +0700

    add schedule

--- src/pages/Kai.tsx
+++ src/pages/Kai.tsx
@@ -1,35 +1,84 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { API_URL } from "../utils/constant";
 
 function Kai() {
-  let date = new Date(Date.now());
-  const useQuery = () => new URLSearchParams(useLocation().search);
-  const query = useQuery();
-  const dateQuery = query.get("date");
-  const { trainSlug } = useParams();
-
   interface DataTrain {
     id: number;
     code: string;
     name: string;
   }
 
+  interface Station {
+    id: number;
+    code: string;
+    name: string;
+    city: string;
+    timezone: string;
+    description: string;
+  }
+
+  interface Schedule {
+    id: number;
+    code: string;
+    trip_date: string;
+    train_code: string;
+    station_code: string;
+    arrival: string;
+    departure: string;
+    station: Station;
+  }
+
+  interface DataTrainSchedule {
+    id: number;
+    code: string;
+    name: string;
+    description: string;
+    schedule: Schedule[];
+  }
+
+  let date = new Date(Date.now());
+  const useQuery = () => new URLSearchParams(useLocation().search);
+  const query = useQuery();
+  const dateQuery = query.get("date");
+  const { trainSlug } = useParams();
+
   const [trains, setTrains] = useState<Array<DataTrain>>([]);
 
-  // console.log(date);
+  const [trainsSchedules, setTrainsSchedules] = useState<
+    Array<DataTrainSchedule>
+  >([]);
 
-  let train = trainSlug?.replace("-", " ").toUpperCase();
+  let train = trainSlug?.replace("-", " ").toLowerCase();
 
   if (dateQuery != null) {
     date = new Date(dateQuery);
   }
 
-  console.log(dateQuery);
-  console.log(train);
-  // the search result
-  // const [foundTrains, setFoundTrains] = useState(TRAINS);
+  const [trainForm, setTrainForm] = useState(train);
+
+  const handleTrainFormChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setTrainForm(event.target.value);
+  };
+
+  const [dateForm, setDateForm] = useState(date.toISOString().slice(0, 10));
+  const handleDateFormChange = (event: React.BaseSyntheticEvent) => {
+    setDateForm(event.target.value);
+  };
+
+  let navigate = useNavigate();
+  const handleSubmit = () => {
+    let path =
+      "/kai/" +
+      trainForm?.replace(" ", "-").toLowerCase() +
+      "?date=" +
+      dateForm;
+    navigate(path);
+    window.location.reload();
+  };
 
   useEffect(() => {
     axios
@@ -40,23 +89,48 @@ function Kai() {
       .catch((err) => console.error(err));
   }, []);
 
-  console.log(trains);
+  useEffect(() => {
+    axios
+      .get(
+        API_URL +
+          "/kai/train-schedule" +
+          "?train=" +
+          trainForm?.replace(" ", "-").toLowerCase() +
+          "&date=" +
+          dateForm
+      )
+      .then((response) => {
+        setTrainsSchedules(response.data.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <main className="container">
       <div className="d-flex align-items-center p-3 my-3 bg-red rounded shadow-sm">
         <div className="container">
           <div className="col-auto float-start">
-            <h1 className="h4">{train}</h1>
+            <h1 style={{ textTransform: "capitalize" }} className="h4">
+              {train}
+            </h1>
           </div>
           <div className="col-auto float-end">
             <div className="row g-2">
               <div className="col-auto">
                 <div className="float-right">
-                  <select className="form-select" aria-label="Kereta Api">
-                    <option value={train}>{train}</option>
+                  <select
+                    onChange={handleTrainFormChange}
+                    className="form-select"
+                    aria-label="Kereta Api"
+                  >
+                    <option
+                      style={{ textTransform: "capitalize" }}
+                      value={trainForm}
+                    >
+                      {train}
+                    </option>
                     {trains.map((train) => (
-                      <option key={train.id} value={train.id}>
+                      <option key={train.id} value={train.name}>
                         {train.name}
                       </option>
                     ))}
@@ -71,6 +145,7 @@ function Kai() {
                     defaultValue={date.toISOString().slice(0, 10)}
                     // name="begin"
                     placeholder="yyyy-mm-dd"
+                    onChange={handleDateFormChange}
                     // min="1997-01-01"
                     // max="2030-12-31"
                   />
@@ -78,7 +153,11 @@ function Kai() {
               </div>
               <div className="col-auto">
                 <div className="float-right">
-                  <button type="submit" className="btn btn-primary">
+                  <button
+                    onClick={handleSubmit}
+                    type="submit"
+                    className="btn btn-primary"
+                  >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
@@ -100,173 +179,75 @@ function Kai() {
       <div className="my-3 p-3 bg-body rounded shadow-sm">
         <h6 className="border-bottom pb-2 mb-0">Summary</h6>
         <div className="text-body-secondary pt-3">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+          {trainsSchedules[0]?.description != "" ? (
+            <div>{trainsSchedules[0]?.description}</div>
+          ) : (
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </div>
+          )}
         </div>
       </div>
 
       <div className="container my-3 p-3 bg-body rounded shadow-sm">
         <h6 className="border-bottom pb-2 mb-0">Schedule</h6>
         <div className="row">
-          <div className="col-md-3 text-center float-right">
-            <h4>Serayu (104)</h4>
-            <table className="table table-success table-striped">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Station</th>
-                  <th scope="col">Arrival</th>
-                  <th scope="col">Departure</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>19:00</td>
-                  <td>19:08</td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>19:00</td>
-                  <td>19:08</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Larry</td>
-                  <td>19:00</td>
-                  <td>19:08</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div className="col-md-3 text-center float-right">
-            <h4>Serayu (104)</h4>
-            <table className="table table-success table-striped">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Station</th>
-                  <th scope="col">Arrival</th>
-                  <th scope="col">Departure</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>19:00</td>
-                  <td>19:08</td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>19:00</td>
-                  <td>19:08</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Larry</td>
-                  <td>19:00</td>
-                  <td>19:08</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div className="col-md-3 text-center float-right">
-            <h4>Serayu (104)</h4>
-            <table className="table table-success table-striped">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Station</th>
-                  <th scope="col">Arrival</th>
-                  <th scope="col">Departure</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>19:00</td>
-                  <td>19:08</td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>19:00</td>
-                  <td>19:08</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Larry</td>
-                  <td>19:00</td>
-                  <td>19:08</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div className="col-md-3 text-center float-right">
-            <h4>Serayu (104)</h4>
-            <table className="table table-success table-striped">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Station</th>
-                  <th scope="col">Arrival</th>
-                  <th scope="col">Departure</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>19:00</td>
-                  <td>19:08</td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>19:00</td>
-                  <td>19:08</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Larry</td>
-                  <td>19:00</td>
-                  <td>19:08</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          {trainsSchedules.map((trainsSchedule) => (
+            <div
+              key={trainsSchedule.code}
+              className="col-md-4 text-center float-right"
+            >
+              <h4>
+                {trainsSchedule.name} ({trainsSchedule.code})
+              </h4>
+              <table className="table table-success table-striped">
+                <thead>
+                  <tr>
+                    <th scope="col">Station</th>
+                    <th scope="col">Arrival</th>
+                    <th scope="col">Departure</th>
+                  </tr>
+                </thead>
+                {trainsSchedule.schedule != null ? (
+                  trainsSchedule.schedule.map((schedule) => (
+                    <tbody key={schedule.id}>
+                      <tr>
+                        <td className="text-start">{schedule.station.name}</td>
+                        <td>{schedule.arrival.substring(11, 19)}</td>
+                        <td>{schedule.departure.substring(11, 19)}</td>
+                      </tr>
+                    </tbody>
+                  ))
+                ) : (
+                  <tbody>
+                    <tr>
+                      <td>Not Found</td>
+                      <td>Not Found</td>
+                      <td>Not Found</td>
+                    </tr>
+                  </tbody>
+                )}
+              </table>
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div>
-        {/* {trains.map((train) => (
-        <div key={train.id}>
-          <p>{train.id}</p>
-          <p>{train.code}</p>
-          <p>{train.name}</p>
-        </div>
-      ))} */}
       </div>
     </main>
   );
